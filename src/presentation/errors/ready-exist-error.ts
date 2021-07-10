1dcec90fac7e389baf63cddcb2ba58b89986e1be
FidelMaster <real-fidel1999@hotmail.com>
2021-07-10 13:24:23 -0600

    structure app initial

---
+++ src/presentation/errors/ready-exist-error.ts
@@ -0,0 +1,13 @@
+export class ReadyExist extends Error {
+  constructor (paramName: string) {
+    super(`This information already exists in our database: ${paramName}`)
+    this.name = `This information already exists in our database: ${paramName}`
+  }
+}
+
+export class NoReadyExist extends Error {
+  constructor (paramName: string) {
+    super(`This does not exist in our database: ${paramName}`)
+    this.name = `This does not exist in our database: ${paramName}`
+  }
+}

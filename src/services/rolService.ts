142f69d25573d2185b0bbb35163d2aecc2f495dc
FidelMaster <real-fidel1999@hotmail.com>
2021-07-15 21:00:38 -0600

    fix

--- src/services/rolService.ts
+++ src/services/rolService.ts
@@ -5,6 +5,16 @@ class RolService {
   async verifyRolAction (id, endPoint) {
     return RolRepository.find({ _id: id,'actions.end_point': endPoint })
   }
+
+  async create (rol: RoleModel) {
+    return RolRepository.create(rol)
+  }
+
+  async update (id, rol) {
+    return RolRepository.findByIdAndUpdate(id, rol, { new: true, useFindAndModify: true })
+  }
+
+
 }
 
 export default new RolService()
